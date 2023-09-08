2e8d72d0345bcc36881bde9b126b2aba61131add
Randy Kofi Ansah <58630675+Randyansah@users.noreply.github.com>
2023-09-08 10:50:50 +0000

    edited test file

--- test_corenlp.py
+++ test_corenlp.py
@@ -2,11 +2,3 @@ from nlplogic.corenlp import search_wikipedia, summarize_wikipedia,get_text_blob
 
 def test_get_phrase():
     assert 'golden state' in get_phrases("Golden State Warriors")
-
-
-def test_get_phrase():
-    assert 
-
-
-def test_get_phrase():
-    assert 
