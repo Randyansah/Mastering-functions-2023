18654d78c888b4f1950a889e9084e08175cc40ca
Randy Kofi Ansah <58630675+Randyansah@users.noreply.github.com>
2023-09-08 10:43:33 +0000

    Added test file

--- nlplogic/corenlp.py
+++ nlplogic/corenlp.py
@@ -1,4 +1,30 @@
-from textblob import textblob
+from textblob import TextBlob
 import wikipedia
 
-golden_state_warriors=wikipedia.summary("Golden State warriors")
+
+def search_wikipedia(name):
+    print(f"Searching for name{name}")
+    return wikipedia.search(name)
+
+
+def summarize_wikipedia(name):
+    """Summarize wikipedia"""
+    print(f"finding wikipedia summary for name:{name}")
+    return wikipedia.summary(name)
+
+
+def get_text_blob(text):
+    """Getting text blob objects and returns"""
+    blob = TextBlob(text)
+    return blob
+
+
+def get_phrases(name):
+    """Find wikipedia anem and return back phrases"""
+    text = summarize_wikipedia(name)
+    blob = get_text_blob(text)
+    phrases = blob.noun_phrases
+    return phrases
+
+
+golden_state_warriors = wikipedia.summary("Golden State warriors")
---
+++ test_corenlp.py
@@ -0,0 +1,12 @@
+from nlplogic.corenlp import search_wikipedia, summarize_wikipedia,get_text_blob,get_phrases
+
+def test_get_phrase():
+    assert 'golden state' in get_phrases("Golden State Warriors")
+
+
+def test_get_phrase():
+    assert 
+
+
+def test_get_phrase():
+    assert 
